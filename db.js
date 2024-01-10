49c425c9336a78ddcd2910d6a51535b0d9bbc7bc
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-10 14:52:55 +0300

    database connection

--- db.js
+++ db.js
@@ -5,3 +5,6 @@ mongoose.connect('mongodb://localhost/inventory', { userNewUrlparser: true});
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('Database connected successfully');
+});
