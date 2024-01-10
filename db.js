ffaf79db138ac8b99d44c3c72bbbc8745c2dfcdd
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-10 16:52:14 +0300

    Partially working

--- db.js
+++ db.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
+const uri = ''
+const connectDabase = () => {
+    mongoose.connect(process.env.DATABASE);
+    console.log("Database Connected");
+};
 
-mongoose.connect('mongodb://0.0.0.0:27017/', { useUnifiedTopology:true});
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    console.log('Database connected successfully');
-});
+module.exports = connectDabase;
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const db = require('./db');
 const routes = require('./routes');
+const connectDabase = require('./db');
+
+connectDabase();
 
 app.use(bodyParser.json());
 
+app.use(express.urlencoded({ extended: true}));
 app.use('/', routes);
 
 app.listen(port, ()=> console.log(`App listening at http://localhost: ${port}`));
