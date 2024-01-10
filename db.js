7f0b8dddeba89f623b6cdd15526367763f8531fc
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-10 16:35:30 +0300

    Connection is temporary

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/inventory', { useUnifiedTopology:true});
+mongoose.connect('mongodb://0.0.0.0:27017/', { useUnifiedTopology:true});
 
 const db = mongoose.connection;
 
